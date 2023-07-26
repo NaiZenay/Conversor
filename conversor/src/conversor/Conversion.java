6ab99e7f51a9dec09b235d2c4717a8de47da2478
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-25 19:28:16 -0600

    Creando Proyecto

---
+++ conversor/src/conversor/Conversion.java
@@ -0,0 +1,13 @@
+package conversor;
+
+import javax.swing.JOptionPane;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String cantidad = JOptionPane.showInputDialog("Ingresa la cantidad a convertir\n(Este conversor utiliza pesos mexicanos)");
+	}
+
+}
