83dd84cd530bb79df68eced04bfbb1f264d5b0fd
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-25 20:39:55 -0600

    Update Conversion.java
    Cuadro de dialogo con opciones de conversion

--- conversor/src/conversor/Conversion.java
+++ conversor/src/conversor/Conversion.java
@@ -7,7 +7,10 @@ public class Conversion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String [] conversiones= {"Pesos a Dolar","Pesos a Euros","Pesos a Libras","Pesos a Yenes", "Pesos a Won"};
 		String cantidad = JOptionPane.showInputDialog("Ingresa la cantidad a convertir\n(Este conversor utiliza pesos mexicanos)");
+		String tipoConversion = (String) JOptionPane.showInputDialog(null, "Selecciona la conversion","Conversiones",JOptionPane.QUESTION_MESSAGE, null, conversiones, conversiones[0]);
+		  }
 	}
 
-}
+
