78b737eb51af1c7f23a841d4f675e7861203bd89
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-26 08:23:55 -0600

    Validacion de entrada de cantidad y opraciones de conversion

--- conversor/src/conversor/Conversion.java
+++ conversor/src/conversor/Conversion.java
@@ -1,16 +1,55 @@
 package conversor;
 
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 public class Conversion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		DecimalFormat df=new DecimalFormat("#.00");
 
-		String [] conversiones= {"Pesos a Dolar","Pesos a Euros","Pesos a Libras","Pesos a Yenes", "Pesos a Won"};
-		String cantidad = JOptionPane.showInputDialog("Ingresa la cantidad a convertir\n(Este conversor utiliza pesos mexicanos)");
-		String tipoConversion = (String) JOptionPane.showInputDialog(null, "Selecciona la conversion","Conversiones",JOptionPane.QUESTION_MESSAGE, null, conversiones, conversiones[0]);
-		  }
+		String cantidad;
+		String resultado;
+		String[] conversiones = { "Pesos a Dolar", "Pesos a Euros", "Pesos a Libras", "Pesos a Yenes", "Pesos a Won" };
+
+		//Validacion de registro de cantidad a convertir
+		boolean repetir = true;
+		do {
+			 cantidad = JOptionPane
+					.showInputDialog("Ingresa la cantidad a convertir\n(Este conversor utiliza pesos mexicanos)");
+			if(contieneSoloNumerosRegex(cantidad)) {
+				repetir = false;
+			}
+		} while (repetir);
+
+		String tipoConversion = (String) JOptionPane.showInputDialog(null, "Selecciona la conversion", "Conversiones",
+				JOptionPane.QUESTION_MESSAGE, null, conversiones, conversiones[0]);
+
+		switch (tipoConversion) {
+		case "Pesos a Dolar":
+			Double cantidadConvetidaDolar = Double.valueOf(cantidad);
+			df.format(cantidadConvetidaDolar/=16.87);
+			resultado=cantidad + " equivale a " + cantidadConvetidaDolar +" Dolares";
+			JOptionPane.showMessageDialog(null, resultado, "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
+			break;
+			
+		case "Pesos a Euros":
+			Double cantidadConvetidaEuro = Double.valueOf(cantidad);
+			df.format(cantidadConvetidaEuro/=18.69);
+			resultado=cantidad + " equivale a " + cantidadConvetidaEuro +" Euro";
+			JOptionPane.showMessageDialog(null, resultado, "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
+			break;
+
+		default:
+			break;
+		}
+
 	}
 
-
+	public static boolean contieneSoloNumerosRegex(String cadena) {
+		return cadena.matches("[0-9]+");
+	}
+}
